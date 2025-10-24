09c42a9d74bc0f2fbf94639bedc4ddb3dcbe0cb6
Lamont Labs <LamontLabs@proton.me>
2025-10-24 13:13:12 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
+// Project-level Gradle
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+  id("com.android.application") version "8.5.0" apply false
+  id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
+tasks.register<Delete>("clean") { delete(rootProject.buildDir) }
